705dc4ad04f99c8344382744224af792bfd69a0c
abdishakur shakib <a.shakib.abubaker@gmail.com>
2019-07-24 21:18:25 +0200

    Add project files.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\AShakib\source\repos\DjangoWebProject8\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
